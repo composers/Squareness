e764f466f852c2cb7b4389bdc39f6b736c24ddeb
Stefan Stolevski <stefanstolevski@Stefans-MacBook-Pro.local>
2014-07-12 00:10:45 +0200

    Initial Commit

---
+++ FSquares/FSQAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FSQAppDelegate.h
+//  FSquares
+//
+//  Created by Stefan Stolevski on 7/12/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FSQAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FSquares/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FSquares
+//
+//  Created by Stefan Stolevski on 7/12/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FSQAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FSQAppDelegate class]));
+    }
+}
